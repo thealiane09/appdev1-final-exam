536589fac354b0bfdec678d8b6ff079d33c5fa96
Yummii09 <inalealtia15@gmail.com>
2025-11-29 13:08:08 +0800

    Implement Home component

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function Home () {
+  return (
+    <div>
+      <h1>Welcome to the Todo App</h1>
+      <Link to="/login">Go to Login</Link>
+    </div>
+  )
+}
+
+export default Home
